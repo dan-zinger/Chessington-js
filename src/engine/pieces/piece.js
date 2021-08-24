08eaca9450cdacd978c3e7e1218dc62a2c208e01
caramonica <caramonica@hotmail.co.uk>
2021-08-24 16:43:18 +0100

    added static object directions

--- src/engine/pieces/piece.js
+++ src/engine/pieces/piece.js
@@ -1,14 +1,25 @@
+import Player from "../player";
+
 export default class Piece {
-    constructor(player) {
-        this.player = player;
-    }
+  constructor(player) {
+    this.player = player;
+  }
 
-    getAvailableMoves(board) {
-        throw new Error('This method must be implemented, and return a list of available moves');
-    }
+  static directions = {
+    UP: 1,
+    DOWN: -1,
+    RIGHT: 1,
+    LEFT: -1,
+  };
 
-    moveTo(board, newSquare) {
-        const currentSquare = board.findPiece(this);
-        board.movePiece(currentSquare, newSquare);
-    }
+  getAvailableMoves(board) {
+    throw new Error(
+      "This method must be implemented, and return a list of available moves"
+    );
+  }
+
+  moveTo(board, newSquare) {
+    const currentSquare = board.findPiece(this);
+    board.movePiece(currentSquare, newSquare);
+  }
 }
