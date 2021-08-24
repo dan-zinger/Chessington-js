518d424bc76018f51eba92d67eaab8601971f22e
caramonica <caramonica@hotmail.co.uk>
2021-08-24 16:41:24 +0100

    passes given tests

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,18 +1,33 @@
-import Player from '../player';
-import Square from '../square';
-import Piece from './piece';
+import Player from "../player";
+import Square from "../square";
+import Piece from "./piece";
 
 export default class Pawn extends Piece {
-    constructor(player) {
-        super(player);
-    }
+  constructor(player) {
+    super(player);
+  }
 
-    getAvailableMoves(board) {
-        let location = board.findPiece(this)
-        if (this.player === Player.WHITE) {
-            return Square.at(location.row + 1, location.col)
-        } else {
-            return Square.at(location.row - 1, location.col)
-        }
+  getAvailableMoves(board) {
+    let location = board.findPiece(this);
+    if (this.player === Player.WHITE) {
+      if (location.row === 1) {
+        return [
+          Square.at(location.row + 2, location.col),
+          Square.at(location.row + 1, location.col),
+        ];
+      } else {
+        return [Square.at(location.row + 1, location.col)];
+      }
+      // we want to add an if statement to take into account the row of the current position
+    } else {
+      if (location.row === 6) {
+        return [
+          Square.at(location.row - 2, location.col),
+          Square.at(location.row - 1, location.col),
+        ];
+      } else {
+        return [Square.at(location.row - 1, location.col)];
+      }
     }
+  }
 }
