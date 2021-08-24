938e10b2e07b950e34b5d018733de30aa2ef2011
caramonica <caramonica@hotmail.co.uk>
2021-08-24 18:28:19 +0100

    cleaning

--- src/engine/pieces/piece.js
+++ src/engine/pieces/piece.js
@@ -1,16 +1,20 @@
 import Player from "../player";
 
+const directions = {
+  UP: { increment: 1, isVertical: true },
+  DOWN: { increment: -1, isVertical: true },
+  RIGHT: { increment: 1, isVertical: false },
+  LEFT: { increment: -1, isVertical: false },
+};
+
 export default class Piece {
   constructor(player) {
     this.player = player;
   }
 
-  static directions = {
-    UP: 1,
-    DOWN: -1,
-    RIGHT: 1,
-    LEFT: -1,
-  };
+  static get directions() {
+    return directions;
+  }
 
   getAvailableMoves(board) {
     throw new Error(
