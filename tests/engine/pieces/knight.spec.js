3339d3817c55a3b7de37451c657422e1d6db2872
caramonica <caramonica@hotmail.co.uk>
2021-08-26 11:41:00 +0100

    same error fixed in bishop and knight specs

--- tests/engine/pieces/knight.spec.js
+++ tests/engine/pieces/knight.spec.js
@@ -3,7 +3,7 @@ import Knight from '../../../src/engine/pieces/knight';
 import Board from '../../../src/engine/board';
 import Player from '../../../src/engine/player';
 import Square from '../../../src/engine/square';
-import Pawn from "../../../src/engine/square";
+import Pawn from "../../../src/engine/pieces/pawn";
 
 describe('Knight', () => {
 
