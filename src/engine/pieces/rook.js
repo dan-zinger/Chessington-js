9c19caba7b2fdc3f9c94cd0ba75e4b1727c73636
caramonica <caramonica@hotmail.co.uk>
2021-08-26 10:32:42 +0100

    rook refactor

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -7,30 +7,14 @@ export default class Rook extends Piece {
     super(player);
   }
 
-  //   getRookMovesOld(board, location, direction) {
-  //     let moves = [];
-  //     let position = [location.row, location.col]
-  //     while (true) {
-  //         position[0] += direction.up
-  //         let square = Square.at(...position)
-  //         let squareContents = board.getPiece(square)
-  //         if (!squareContents) {
-  //             break;
-  //         } else if (this.player === squareContents.player) {
-
-  //         }
-  //     }
-
-  //     return moves
-  //   }
-
   getMovesInDirection(board, direction) {
     const location = board.findPiece(this);
-
     const moves = [];
+
     const position = [location.row, location.col];
     const positionIndex = direction.isVertical ? 0 : 1;
     position[positionIndex] += direction.increment;
+
     let square = Square.at(...position);
 
     while (Rook.isOnBoard(square) && !this.isOccupiedByOwnTeam(board, square)) {
@@ -44,8 +28,8 @@ export default class Rook extends Piece {
 
   getAvailableMoves(board) {
     return Object.entries(Rook.directions)
-      .map(directionKeyValuePair =>
-        this.getMovesInDirection(board, directionKeyValuePair[1])
+      .map(([, directionValue]) =>
+        this.getMovesInDirection(board, directionValue)
       )
       .flat();
   }
